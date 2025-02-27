9d0e71fef8bd19ee45fd75ec63aa7a5e269b2550
bernabarros <bernardobarros.work@gmail.com>
2025-02-27 00:05:26 +0000

    Created SmoothOperator project and code

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string numero_int;
+            sbyte n;
+
+            Console.Write("Insere um número inteiro: ");
+            numero_int = Console.ReadLine();
+            n = sbyte.Parse(numero_int);
+            n = ++n;
+            Console.WriteLine($"{n}");
+            n = --n;
+            Console.WriteLine($"{n}");
+
+        }
+    }
+}
